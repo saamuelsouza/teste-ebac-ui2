589f0a36ec8b7f546312fff79147a9fc7101875f
Samuel Souza <saamuelfelipe@gmail.com>
2024-01-19 21:27:45 -0300

    Alterações utilizando massa de dados

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -12,17 +12,31 @@ describe('Funcionalidade: produtos', () => {
          cy.get('#tab-title-description > a').should('contain', 'Descrição')
     });
 
-    it.only('Deve buscar um produto com sucesso', () => {
+    it('Deve buscar um produto com sucesso', () => {
         let produto = 'Zeppelin Yoga Pant'
         produtosPage.buscarProduto(produto)
         cy.get('.product_title').should('contain', produto)
     });
 
     it('Deve visitar a página do produto', () => {
-        
+        produtosPage.visitarProduto('Zeppelin-Yoga-Pant')
     });
 
-    it('Deve aadicionar produto ao carrinho', () => {
-        
+    it('Deve adicionar produto ao carrinho', () => {
+        let qtd = 7
+        produtosPage.buscarProduto('Abominable Hoodie')
+        produtosPage.addProdutoCarrinho('M', 'Green', qtd)
+        cy.get('.woocommerce-message').should('contain', qtd + ' × “Abominable Hoodie” foram adicionados no seu carrinho.')
+    });
+
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+            produtosPage.buscarProduto(dados[1].nomeProduto)
+            produtosPage.addProdutoCarrinho(
+                dados[1].tamanho, 
+                dados[1].cor, 
+                dados[1].quantidade)
+            cy.get('.woocommerce-message').should('contain', dados[1].nomeProduto)
+        })
     });
 });
